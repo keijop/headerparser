4f88aa2911caa608abc50d2a89f8164eba5e4a7d
keijop <76419484+keijop@users.noreply.github.com>
2021-07-12 10:27:33 +0300

    Update whoami.js

--- controller/whoami.js
+++ controller/whoami.js
@@ -1,7 +1,7 @@
 const whoami = (req, res) => {
 	console.log(req)
 	return res.status(200).json({ ipaddress : req.ip , software : req.headers['user-agent'],
-	language :  req.headers.accept })
+	language :  req.headers['accept-language'] })
 } 
 
 module.exports = whoami
